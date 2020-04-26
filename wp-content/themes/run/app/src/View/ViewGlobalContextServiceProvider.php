0ae1dcbc91ea52f6b5c5ae58668bb027e5e61ad6
Janosense <janosense@gmail.com>
2020-04-26 23:53:22 +0300

    Added canonical url

--- wp-content/themes/run/app/src/View/ViewGlobalContextServiceProvider.php
+++ wp-content/themes/run/app/src/View/ViewGlobalContextServiceProvider.php
@@ -20,12 +20,14 @@ class ViewGlobalContextServiceProvider implements ServiceProviderInterface {
 		$theme_version = wp_get_theme()->get( 'Version' );
 		$is_admin      = current_user_can( 'manage_options' );
 		$site_title    = get_bloginfo( 'name' );
+		$canonical     = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? "https" : "http" ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 		View::addGlobals( [
 			'routes'        => $routes,
 			'theme_version' => $theme_version,
 			'is_admin'      => $is_admin,
 			'site_title'    => $site_title,
+			'canonical'     => $canonical,
 		] );
 	}
 }
