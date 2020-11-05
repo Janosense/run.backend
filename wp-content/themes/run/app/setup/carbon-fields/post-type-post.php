79671fd85416d73a0603848a01f6057a642958bb
Janosense <janosense@gmail.com>
2020-11-06 00:03:42 +0200

    Changed position of CRB meta fields for post

--- wp-content/themes/run/app/setup/carbon-fields/post-type-post.php
+++ wp-content/themes/run/app/setup/carbon-fields/post-type-post.php
@@ -5,6 +5,7 @@ use Carbon_Fields\Field;
 
 Container::make( 'post_meta', 'meta_data', __( 'Post meta data', 'run' ) )
          ->where( 'post_type', '=', 'post' )
+         ->set_context( 'side' )
          ->add_fields( [
 	         Field::make( 'text', 'crb_meta_description', __( 'Post meta description', 'run' ) ),
 	         Field::make( 'image', 'crb_meta_image', __( 'Post meta image', 'run' ) )->set_value_type( 'url' ),
