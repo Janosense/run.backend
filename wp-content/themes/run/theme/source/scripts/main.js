7b7ce5015a983055293e01aef63b32d8b4921f93
Janosense <janosense@gmail.com>
2022-09-27 13:37:29 +0300

    Test fetch FA

--- wp-content/themes/run/theme/source/scripts/main.js
+++ wp-content/themes/run/theme/source/scripts/main.js
@@ -8,3 +8,11 @@ init_sticky_main_nav();
 toggle_main_nav();
 init_glightbox();
 toggle_shoes_list();
+
+fetch('https://teamfeed.feedingamerica.org/api/1.3/participants/10072?_=1664203236941')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
